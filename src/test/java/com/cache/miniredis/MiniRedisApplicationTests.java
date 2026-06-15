bd3a39564748759360d03c6b5759d06dc2c0724b
Rahul <rahul@sniper.local>
2026-06-15 12:10:35 +0000

    chore: initial spring boot project setup

---
+++ src/test/java/com/cache/miniredis/MiniRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cache.miniredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
